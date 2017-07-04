c60732c27b78fa4cf69d93abd82e72d1562e9152
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-07-04 08:43:52 -0700

    Add consts module for static/constant values.

---
+++ adbwp/consts.py
@@ -0,0 +1,21 @@
+"""
+    adbwp.consts
+    ~~~~~~~~~~~~
+
+    Contains const values used by the protocol.
+"""
+
+#: Protocol version.
+VERSION = 0x01000000
+
+#: Maximum message body size.
+MAXDATA = 256 * 1024
+
+#: Older ADB version max data size limit; required max for CONNECT and AUTH messages.
+CONNECT_AUTH_MAXDATA = 4096
+
+#: Size of a serialized ADB message in bytes.
+MESSAGE_SIZE = 24
+
+#: Bitmask applied to the "magic" value of ADB messages.
+COMMAND_MASK = 0xffffffff
