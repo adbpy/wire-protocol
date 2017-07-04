372618ad1a291920944c351a450fda3c2fd751e6
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-07-04 13:00:24 -0700

    Add tests for header properties that check against the command.

--- tests/test_header.py
+++ tests/test_header.py
@@ -6,9 +6,61 @@
 """
 import pytest
 
-from adbwp import header
+from adbwp import enums, header
 
 
-@pytest.mark.xfail(reason='Not Implemented')
-def test_stub():
-    assert False
+#: Names of properties on the :class:`~adbwp.header.Header` class that shortcut checks for determining
+#: the command type.
+#: Note: This order must match the enum value definition order of :class:`~adbwp.enums.Command`.
+HEADER_COMMAND_PROPERTY_NAMES = ['sync', 'connect', 'auth', 'open', 'ready', 'close', 'write']
+
+
+#: Names of properties on the :class:`~adbwp.header.Header` class that shortcut checks for determining
+#: the command response type.
+#: Note: This order must match the enum value definition order of :class:`~adbwp.enums.CommandResponse`.
+HEADER_COMMAND_RESPONSE_PROPERTY_NAMES = ['okay', 'fail']
+
+
+@pytest.fixture(scope='session', params=list(zip(HEADER_COMMAND_PROPERTY_NAMES, enums.Command)))
+def property_name_with_command(request):
+    """
+    Fixture that yields valid command types paired with the name of the property defined on
+    :class:`~adbwp.header.Header` that checks it.
+    """
+    return request.param
+
+
+@pytest.fixture(scope='session', params=list(zip(HEADER_COMMAND_RESPONSE_PROPERTY_NAMES, enums.CommandResponse)))
+def property_name_with_command_response(request):
+    """
+    Fixture that yields valid command response types paired with the name of the property defined on
+    :class:`~adbwp.header.Header` that checks it.
+    """
+    return request.param
+
+
+def test_header_property_check_matches_command_type(property_name_with_command):
+    """
+    Assert that the properties defined on :class:`~adbwp.header.Header` that check the command value
+    return :bool:`~True` as expected.
+    """
+    name, value = property_name_with_command
+    instance = _header(value)
+    assert getattr(instance, name)
+
+
+def test_header_property_check_matches_command_response_type(property_name_with_command_response):
+    """
+    Assert that properties defined on :class:`~adbwp.header.Header` that check the command response value
+    return :bool:`~True` as expected.
+    """
+    name, value = property_name_with_command_response
+    instance = _header(value)
+    assert getattr(instance, name)
+
+
+def _header(command=None, arg0=None, arg1=None, data_length=None, data_checksum=None, magic=None):
+    """
+    Helper function that creates :class:`~adbwp.header.Header` instances with default values.
+    """
+    return header.Header(command, arg0, arg1, data_length, data_checksum, magic)
