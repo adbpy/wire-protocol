1702cca5bd207d90a796b5cc4fc61d4d574be929
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-07-04 09:59:21 -0700

    Add test module for header module.

---
+++ tests/test_header.py
@@ -0,0 +1,14 @@
+"""
+    test_header
+    ~~~~~~~~~~~
+
+    Contains tests for the :mod:`~adbwp.header` module.
+"""
+import pytest
+
+from adbwp import header
+
+
+@pytest.mark.xfail(reason='Not Implemented')
+def test_stub():
+    assert False
