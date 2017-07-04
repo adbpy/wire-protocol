4f516020c8da25de20a846613c14848596bbada8
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-07-04 08:44:27 -0700

    Add enums module for enumeration types.
    In the future, consider refactoring this and merging into
    a single module, e.g. model.py.

---
+++ adbwp/enums.py
@@ -0,0 +1,47 @@
+"""
+    adbwp.enums
+    ~~~~~~~~~~~
+
+    Contains enumeration types used by the protocol.
+"""
+import enum
+
+
+class Command(enum.IntEnum):
+    """
+    Enumeration for command types used by the ADB protocol.
+    """
+    SYNC = 0x434e5953  # Internal only.
+    CNXN = 0x4e584e43
+    AUTH = 0x48545541
+    OPEN = 0x4e45504f
+    OKAY = 0x59414b4f
+    CLSE = 0x45534c43
+    WRTE = 0x45545257
+
+
+class CommandResponse(enum.Enum):
+    """
+    Enumeration for response message types from ADB connection requests.
+    """
+    OKAY = 'OKAY'
+    FAIL = 'FAIL'
+
+
+class AuthType(enum.IntEnum):
+    """
+    Enumeration for authentication types used by the ADB protocol.
+    """
+    TOKEN = 1
+    SIGNATURE = 2
+    RSA_PUBLIC_KEY = 3
+
+
+class SystemType(enum.Enum):
+    """
+    Enumeration for "systemtype" values used by the ADB protocol for the "system-identity-string"
+    value in a CONNECT message.
+    """
+    BOOTLOADER = 'bootloader'
+    DEVICE = 'device'
+    HOST = 'host'
