1cf40e5a009d67fd20aea6b6d00e94b779187195
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-07-04 18:42:35 -0700

    Add tests for header to/from bytes calls.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,9 @@
     High level fixtures used across multiple test modules.
 """
 import pytest
+import random
 
-from adbwp import enums
+from adbwp import enums, header
 
 
 @pytest.fixture(scope='session', params=enums.Command)
@@ -15,3 +16,69 @@ def command_type(request):
     Fixture that yields all :class:`~adbwp.enums.Command` types.
     """
     return request.param
+
+
+@pytest.fixture(scope='session')
+def random_header(command_type, random_arg0, random_arg1, random_data_length,
+                  random_data_checksum, command_type_magic):
+    """
+    Fixture that yields a :class:`~adbwp.header.Header` instance for each :enum:`~adbwp.enums.Command`
+    type and with random values.
+    """
+    return header.new(command_type, random_arg0, random_arg1, random_data_length,
+                      random_data_checksum, command_type_magic)
+
+
+@pytest.fixture(scope='session')
+def random_header_bytes(random_header):
+    """
+    Fixture that yields a :class:`~bytes` instance that represents a header.
+    """
+    return header.to_bytes(random_header)
+
+
+@pytest.fixture(scope='session')
+def command_type_magic(command_type):
+    """
+    Fixture that yields the computed "magic" value of the given command type.
+    """
+    return header.magic(command_type)
+
+
+@pytest.fixture(scope='session')
+def random_arg0():
+    """
+    Fixture that yields a random integer value that is usable as "arg0" in a :class:`~adbwp.header.Header`.
+    """
+    return random_int()
+
+
+@pytest.fixture(scope='session')
+def random_arg1():
+    """
+    Fixture that yields a random integer value that is usable as "arg1" in a :class:`~adbwp.header.Header`.
+    """
+    return random_int()
+
+
+@pytest.fixture(scope='session')
+def random_data_length():
+    """
+    Fixture that yields a random integer value that is usable as "data_length" in a :class:`~adbwp.header.Header`.
+    """
+    return random_int()
+
+
+@pytest.fixture(scope='session')
+def random_data_checksum():
+    """
+    Fixture that yields a random integer value that is usable as "data_checksum" in a :class:`~adbwp.header.Header`.
+    """
+    return random_int()
+
+
+def random_int(x=0, y=2**31 - 1):
+    """
+    Helper function that generates a random integer between two values (inclusive).
+    """
+    return random.randint(x, y)
--- tests/test_header.py
+++ tests/test_header.py
@@ -4,9 +4,10 @@
 
     Contains tests for the :mod:`~adbwp.header` module.
 """
+import os
 import pytest
 
-from adbwp import consts, enums, header
+from adbwp import consts, enums, exceptions, header
 
 
 #: Names of properties on the :class:`~adbwp.header.Header` class that shortcut checks for determining
@@ -39,6 +40,38 @@ def property_name_with_command_response(request):
     return request.param
 
 
+@pytest.fixture(scope='session', params=[
+    float(),
+    list(),
+    tuple(),
+    dict(),
+    set(),
+    None
+])
+def invalid_field_value(request):
+    """
+    Fixture that yields values that are not the correct type for field values
+    of a :class:`~adbwp.header.Header`.
+    """
+    return request.param
+
+
+@pytest.fixture(scope='session', params=range(0, 23))
+def invalid_bytes_too_few(request):
+    """
+    Fixture that yields :class:`~bytes` with a length less than 24.
+    """
+    return os.urandom(request.param)
+
+
+@pytest.fixture(scope='session', params=range(25, 32))
+def invalid_bytes_too_many(request):
+    """
+    Fixture that yields :class:`~bytes` with a length greater than 24.
+    """
+    return os.urandom(request.param)
+
+
 def test_header_property_check_matches_command_type(property_name_with_command):
     """
     Assert that the properties defined on :class:`~adbwp.header.Header` that check the command value
@@ -64,3 +97,66 @@ def test_header_magic_computes_command_xor(command_type):
     Assert that :func:`~adbwp.header.magic` XOR's the command value will :attr:`~adbwp.consts.COMMAND_MASK`.
     """
     assert header.magic(command_type) == command_type ^ consts.COMMAND_MASK
+
+
+def test_header_to_bytes_returns_24_bytes(random_header):
+    """
+    Assert that :func:`~adbwp.header.to_bytes` converts the given :class:`~adbwp.header.Header` to
+    a collection of 24 bytes.
+    """
+    to_bytes = header.to_bytes(random_header)
+    assert len(to_bytes) == 24
+
+
+def test_header_to_bytes_raises_on_header_with_invalid_field_type(command_type, invalid_field_value):
+    """
+    Assert that :func:`~adbwp.header.to_bytes` raises a :class:`~adbwp.exceptions.PackError` when
+    the :class:`~adbwp.header.Header` contains field values that are not integers.
+    """
+    with pytest.raises(exceptions.PackError):
+        header.to_bytes(header.new(command_type, arg0=invalid_field_value))
+
+
+def test_header_to_bytes_raises_on_header_with_integer_overflow(command_type):
+    """
+    Assert that :func:`~adbwp.header.to_bytes` raises a :class:`~adbwp.exceptions.PackError` when
+    the :class:`~adbwp.header.Header` contains an integer field value greater than 32-bits.
+    """
+    with pytest.raises(exceptions.PackError):
+        header.to_bytes(header.new(command_type, arg0=2**32 + 1))
+
+
+def test_header_from_bytes_raises_on_less_than_24_bytes(invalid_bytes_too_few):
+    """
+    Assert that :func:`~adbwp.header.from_bytes` raises a :class:`~adbwp.exceptions.UnpackError` when
+    given a :class:`~bytes` instance that is less than 24 bytes long.
+    """
+    with pytest.raises(exceptions.UnpackError):
+        header.from_bytes(invalid_bytes_too_few)
+
+
+def test_header_from_bytes_raises_on_more_than_24_bytes(invalid_bytes_too_many):
+    """
+    Assert that :func:`~adbwp.header.from_bytes` raises a :class:`~adbwp.exceptions.UnpackError` when
+    given a :class:`~bytes` instance that is more than 24 bytes long.
+    """
+    with pytest.raises(exceptions.UnpackError):
+        header.from_bytes(invalid_bytes_too_many)
+
+
+def test_header_from_bytes_converts_command_to_enum(random_header_bytes):
+    """
+    Assert that :func:`~adbwp.header.from_bytes` converts the command bytes to a
+    :enum:`~adbwp.enums.Command` instance.
+    """
+    instance_from_bytes = header.from_bytes(random_header_bytes)
+    assert isinstance(instance_from_bytes.command, enums.Command)
+
+
+def test_header_converts_to_from_bytes(random_header, random_header_bytes):
+    """
+    Assert that a :class:`~adbwp.header.Header` that is converted to :class:`~bytes` and back
+    maintains fidelity to the original instance.
+    """
+    instance_from_bytes = header.from_bytes(random_header_bytes)
+    assert instance_from_bytes == random_header
