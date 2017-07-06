4c6d9e5c229db05fd5b79299311bf72e9d0aba28
Andrew Hawker <andrew.r.hawker@gmail.com>
2017-07-05 22:22:15 -0700

    Add type hints for data payloads and system type values.

--- adbwp/hints.py
+++ adbwp/hints.py
@@ -11,3 +11,12 @@ from . import enums
 
 #: Type hint that defines multiple types that represent a command.
 Command = typing.Union[int, enums.Command]  # pylint: disable=invalid-name
+
+
+#: Type hint that defines multiple types that can represent a system type
+#: used in a connect message.
+SystemType = typing.Union[str, enums.SystemType]  # pylint: disable=invalid-name
+
+
+#: Type hint that defines multiple types that can represent a message data payload.
+Payload = typing.Union[bytes, bytearray, str, memoryview]  # pylint: disable=invalid-name
