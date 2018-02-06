b2b80d957cfd89d3c56438f9d1a553cb157c2f62
Andrew Hawker <andrew.r.hawker@gmail.com>
2018-02-05 22:34:34 -0800

    Expose 'Message' and 'Header' classes to public API.

--- adbwp/__init__.py
+++ adbwp/__init__.py
@@ -9,7 +9,9 @@
 from . import exceptions
 from .exceptions import *
 from . import header
+from .header import Header
 from . import message
+from .message import Message
 
-__all__ = exceptions.__all__ + ['header', 'message']
+__all__ = exceptions.__all__ + ['header', 'message', 'Header', 'Message']
 __version__ = '0.0.1'
